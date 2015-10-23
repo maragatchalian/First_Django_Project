1249fbbc3fa4203af8e1af11168108832bea44a1
mara <maragatchalian@github.com>
2015-10-23 12:35:03 +0800

    Future date and time

--- First_Django_Project/urls.py
+++ First_Django_Project/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from First_Django_Project.views import hello, current_datetime
+from First_Django_Project.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
--- First_Django_Project/views.py
+++ First_Django_Project/views.py
@@ -8,3 +8,12 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body> It is now %s. </body></html>" % now
     return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    date_time = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body><html>" % (offset, date_time)
+    return HttpResponse(html)
